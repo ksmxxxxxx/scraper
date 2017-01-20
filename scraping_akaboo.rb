5490bc013ffbe42c8de253d341e96f1f95de5039
longicorn <longicorn.c@gmail.com>
2017-01-20 22:27:28 +0900

    メソッドを外出し

--- scraping_akaboo.rb
+++ scraping_akaboo.rb
@@ -4,6 +4,24 @@ require "robotex"
 require "sinatra"
 require "sinatra/reloader"
 
+def event_location(doc)
+  doc.xpath("//table[5]/tr").each do |tr|
+    if !tr.attributes["bgcolor"].nil? && tr.attributes["bgcolor"].value == "#ffc6c6"
+      location = tr.children.search("td").first.children.search("font").children.search("b").text
+      return location
+    end
+  end
+end
+
+def regist_space(doc)
+  doc.xpath("//table[5]/tr").each do |tr|
+    if tr.attributes["height"].value == "40"
+      space_cont = tr.children.search("td[@rowspan]").text
+      return space_cont
+    end
+  end
+end
+
 get '/view_akaboo' do
   robotex = Robotex.new
   robotex.allowed?("http://www.akaboo.jp/event/")
@@ -19,27 +37,9 @@ get '/view_akaboo' do
   doc = Nokogiri::HTML.parse(html, nil, charset)
 
   html = "<h1>イベント一覧</h1>"
-
-  def event_location(doc)
-    doc.xpath("//table[5]/tr").each do |tr|
-      if !tr.attributes["bgcolor"].nil? && tr.attributes["bgcolor"].value == "#ffc6c6"
-        location = tr.children.search("td").first.children.search("font").children.search("b").text
-        return location
-      end
-    end
-  end
   
-  puts event_location
+  puts event_location(doc)
+  puts regist_space(doc)
 
-  def regist_space(doc)
-    doc.xpath("//table[5]/tr").each do |tr|
-      if tr.attributes["height"].value == "40"
-        space_cont = tr.children.search("td[@rowspan]").text
-        return space_cont
-      end
-    end
-  end
-
-  puts regist_space
   return html
 end
