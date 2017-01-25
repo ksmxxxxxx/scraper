9fe93de724f2716e1ea96c7431b98c1c416afa90
longicorn <longicorn.c@gmail.com>
2017-01-25 23:41:15 +0900

    テーブルをクラス化

--- scraping_akaboo.rb
+++ scraping_akaboo.rb
@@ -3,6 +3,28 @@ require "nokogiri"
 
 require 'pp'
 
+class Table
+  def initialize(table)
+    @table = table
+  end
+
+  def header?
+    @table.attributes.has_key?('bgcolor')
+  end
+
+  def data?
+    !header?
+  end
+
+  def line
+    if header?
+      @table.search("td").search("b").map{|attr|attr.text}
+    else
+      @table.search("td").map{|attr|attr.text}
+    end
+  end
+end
+
 url = 'http://www.akaboo.jp/event/'
 user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36'
 charset = nil
@@ -15,10 +37,11 @@ doc = Nokogiri::HTML.parse(html, nil, charset)
 
 lines = []
 doc.xpath("//table[5]").search("tr").each do |table|
-  if table.attributes.has_key?('bgcolor')
-    lines << [:header, table.search("td").search("b").map{|attr|attr.text}]
+  table = Table.new(table)
+  if table.header?
+    lines << [:header, table.line]
   else
-    lines << [:body, table.search("td").map{|attr|attr.text}]
+    lines << [:body, table.line]
   end
 end
 pp lines
