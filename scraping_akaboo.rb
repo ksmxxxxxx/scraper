282fbf98c42b37155c4f768c23a8ffb5605fc58e
longicorn <longicorn.c@gmail.com>
2017-01-25 23:30:57 +0900

    最低限動くようにする

--- scraping_akaboo.rb
+++ scraping_akaboo.rb
@@ -1,10 +1,5 @@
-require "table_parser"
 require "open-uri"
 require "nokogiri"
-require "robotex"
-
-robotex = Robotex.new
-robotex.allowed?("http://www.akaboo.jp/event/")
 
 url = 'http://www.akaboo.jp/event/'
 user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36'
